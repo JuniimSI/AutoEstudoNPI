b90094ba90ed8eee3a5889cfea7e9c87b3e92371
JuniimSI <jrfernandesti@gmail.com>
2017-04-05 15:15:29 -0300

    tudo nova branch

--- src/main/java/com/example/Desafior5Application.java
+++ src/main/java/com/example/Desafior5Application.java
@@ -1,9 +1,13 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class Desafior5Application {
 
 	public static void main(String[] args) {
